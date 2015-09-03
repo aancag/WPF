e0de745767f0be51226d3a0f483bc9d197ea46dd
anca1ant <antonie.anca@yahoo.com>
2015-09-04 00:43:17 +0300

    add

---
+++ RoleViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FirstWPF
+{
+    public class RoleViewModel
+    {
+        public List<RoleModel> roles { get; set; }
+        public List<int> selectedRoles{get; set;}
+        private bool _canExecute;
+        
+        private ICommand _clickCommand;
+        public ICommand ClickCommand
+        {
+            get
+            {
+                return _clickCommand ?? (_clickCommand = new CommandHandler(() => MyAction(), _canExecute));
+            }
+        }
+        
+        public RoleViewModel(List<RoleModel> roles)
+        {
+            this.roles = roles;
+            _canExecute = true;
+        }
+        
+        
+        public void MyAction()
+        {
+            selectedRoles = new List<int>();
+            foreach (var role in roles) {
+                if (role.IsChecked == true) {
+                    selectedRoles.Add(role.Id);
+                }
+            }
+            Application.Current.MainWindow.Close();
+
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWPF
+{
+    public class Startup
+    {
+        public Startup() {
+            var mine = new List<RoleModel>();
+            mine.Add(new RoleModel { Id = 0, Name = "anca"});
+            mine.Add(new RoleModel { Id = 1, Name = "test1" });
+            mine.Add(new RoleModel { Id = 2, Name = "test2" });
+            mine.Add(new RoleModel { Id = 3, Name = "test3" });
+
+            RoleViewModel roleViewModel = new RoleViewModel(mine);
+            var main = new MainWindow();
+            main.DataContext = roleViewModel;
+            main.ShowDialog();
+            var test = roleViewModel.selectedRoles;
+        }
+    }
+}
